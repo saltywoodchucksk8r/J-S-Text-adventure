720b367ca55cd0d6490de64ac5824f3cd3dfe5a6
saltywoodchucksk8r <143848941+saltywoodchucksk8r@users.noreply.github.com>
2024-04-08 15:48:46 -0700

    greht

---
+++ T-A.js
@@ -0,0 +1,71 @@
+const story = {
+start : {
+    text : "It's currently 8:pm and Ze is asleep (your name is ze). your alarm makes you wake up. Then you wakes up and realizes hes late for a pizza lovers convention (for some reason they happen at night). You are rushed out of the house and decided to pick an outfit but only has 3",
+    choices : [
+        ["tan1","The meat lovers outfit"],
+        ["tan2","The pizza lovers outfit"],
+        ["tan3","The cheesed-outfit"]
+    ]
+},
+tan1 : {
+ text : "You pick your outfit and you leave the house",
+ choices : [
+    ["tan4","Next"]
+ ]
+},
+tan2 : {
+    text : "You pick your outfit and you leave the house",
+    choices : [
+     ["tan4","Next"]
+ ]
+},
+tan3 : {
+ text : "You pick your outfit and you leave the house",
+ choices : [
+    ["tan4","Next"]
+ ]
+},
+tan4 : {
+ text : "ze starts running across the street and a 5 ton pizza truck hauling an unholy amount of pizza and ze dies",
+ choices : [
+    ["tan5","Next"]
+ ]
+},
+tan5 : {
+    text : "ze opens their eyes Suddenly and realizes they are no longer in their own world",
+    choices : [
+       ["tan6","Next"]
+    ]
+   },
+ tan6 : {
+    text : "ze sees a new world ridden with pizza all over it, the buildings and the houses everywhere you look theres pizza",
+    choices : [
+       ["tan7","Next"]
+    ]
+   },
+tan7 : {
+    text : "You return to your house where you are greeted by your parents who have chef hats on with a weird greasy pizza stench",
+    choices : [
+       ["tan8","Next"]
+    ]
+   },
+tan8 : {
+    text : "You hear a magical noise and a  white beirded man in a yellow cloak with red spots appears in front of you looking confused and sweaty",
+    choices : [
+       ["tan9","Next"]
+    ]
+   },
+tan9 : {
+    text : "The man introduces himself and says <hey Ze im the great wizard Pizzarino",
+    choices : [
+       ["tan10","Next"]
+    ]
+   },
+tan10 : {
+    text : "I need your help, i was chilling in my tower and all of a sudden the evil demon lord pizzaria stole the 8 slices that hold enough power to destroy our world. Luckily I have the pizza staff which is the only last piece stopping him from destroying the world. Please go retrieve our 8 slices and save the world from destroying the world",
+    choices : [
+       ["tan10","Next"]
+    ]
+   },
+
+}

b8a8ac823742bec783164827d1b3c7f9b2e2e8e1
saltywoodchucksk8r <143848941+saltywoodchucksk8r@users.noreply.github.com>
2024-04-09 10:56:01 -0700

    Some more of the story

--- T-A.js
+++ T-A.js
@@ -64,7 +64,53 @@ tan9 : {
 tan10 : {
     text : "I need your help, i was chilling in my tower and all of a sudden the evil demon lord pizzaria stole the 8 slices that hold enough power to destroy our world. Luckily I have the pizza staff which is the only last piece stopping him from destroying the world. Please go retrieve our 8 slices and save the world from destroying the world",
     choices : [
-       ["tan10","Next"]
+       ["tan11","Next"]
+    ]
+   },
+tan11 : {
+      text : `"Will you help me?"`,
+      choices : [
+         ["tan12",`tell Pizzarino that you need to stay here and protect your "family"`]
+         ["tan13",`tell Pizzarino that "you dont want to because you're in a new world with new people and a whole lot of pizza"`]
+         ["tan14",`tell Pizzarino no and that you want to explore the world on your own`]
+         ["tan15",`ask  pizzarino what he knows about you`]
+    ]
+   },
+tan12 : {
+      text : `Pizzarino says "ok" then walks out and goes off to fight pizzario`,
+      choices : [
+         ["tan16","Next"]
+    ]
+   },
+tan16 : {
+      text : `You never hear from pizzarino again and your new world slowly breaks out into chaos and you see the world open up and swallow everything up and you see pizzario in th edistance casting some sort of spell ad you live for about 10 more seconds before the world explodes killing eveyone including you`,
+      choices : [
+         ["start","Next"]
+    ]
+   },
+tan13 : {
+      text : `Pizzarino says "You have 2 choices, Let everyone in this new world die because you selfishly don't want to even try or you set off to have the adventure of a lifetime"`,
+      choices : [
+         ["tan17","Next"]
+    ]
+   },
+tan17 : {
+      text : `What will you do?`,
+      choices : [
+         ["tan18","Let everyone die"]
+         ["tan19","Set off on an amazing adventure"]
+    ]
+   },
+tan18 : {
+      text : `Pizzarino. utterly shocked unleashes all unholy high hell on you killing you and your family`,
+      choices : [
+         ["start","Next"]
+    ]
+   },
+tan19 : {
+      text : `Pizzarino says "You have 2 choices, Let everyone in this new world die because you selfishly don't want to even try or you set off to have the adventure of a lifetime"`,
+      choices : [
+         ["tan20","Next"]
     ]
    },
 
